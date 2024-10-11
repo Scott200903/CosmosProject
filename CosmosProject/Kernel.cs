94b1f3088c14384f323edbe8207d407ce0c8cc77
scott <info@adrag.de>
2024-10-11 08:42:21 +0200

    Add project files.

---
+++ CosmosProject/Kernel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Sys = Cosmos.System;
+using CosmosProject.UserManagement;
+using System.Reflection.Metadata;
+using System.Diagnostics;
+
+namespace CosmosProject
+{
+	public class Kernel : Sys.Kernel
+	{
+		List<User> tmp;
+		protected override void BeforeRun()
+		{
+			Console.WriteLine("Cosmos booted successfully.");
+
+			tmp = new List<User>();
+		}
+		protected override void Run()
+		{
+			Console.Write("Enter Command: ");
+			string input = Console.ReadLine();
+
+			//Shutdown 
+			if(input == "poweroff" || input == "shutdown") Environment.Exit(0);
+
+			if(input == "adduser")
+			{
+				Console.Write("Enter Username:");
+				string username = Console.ReadLine();
+
+				Console.Write("Enter Vorname:");
+				string vorname = Console.ReadLine();
+
+				Console.Write("Enter Nachname:");
+				string nachname = Console.ReadLine();
+
+				Console.Write("Enter Password:");
+				string password = Console.ReadLine();
+
+				Console.Write("Enter Email:");
+				string email = Console.ReadLine();
+
+				Console.Write("Enter isAdmin:");
+				string isAdmin = Console.ReadLine();
+
+				try
+				{
+					bool tmpadmin = true;
+					User usr = new User(username, vorname, nachname, password, email, tmpadmin);
+					tmp.Add(usr);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.ToString());
+					Thread.Sleep(5000);
+				}s
+			}
+
+			if (input == "listuser")
+			{
+				Console.WriteLine("Username:Vorname:Nachname:Password:Email:tisAdmin");
+				foreach (User usr in tmp)
+				{
+					Console.WriteLine(usr.getUsername() + ":" + usr.getVorname() + ":" + usr.getNachname() + ":" + usr.getPassword() + ":" + usr.getEmail() + ":" + usr.getStatus());
+				}
+
+				Console.WriteLine("Anzahl an Benutzern: " + tmp.Count);
+			}
+			//if (input == "exit")
+			//{
+			//	Console.Write("Do you want to exit the OS?\nType y for yes and n for no!\n");
+
+			//	string exit = Console.ReadLine();
+
+			//	if(exit == "y" || exit == "Y")
+			//	{
+			//		Console.WriteLine("Application stopped!");
+			//		Thread.Sleep(2000);
+
+			//		Environment.Exit(1);
+			//	}
+			//	else if(exit == "n" || exit == "N")
+			//	{
+			//		Console.WriteLine("Application not stopped!");
+			//	}
+			//}
+			//else
+			//{
+			//	Console.WriteLine(input);
+			//}
+		}
+	}
+}
---
+++ CosmosProject/UserManagement/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosProject.UserManagement
+{
+	public class User
+	{
+		private string vorname;
+		private string nachname;
+		private string username;
+		private string password;
+		private string email;
+		private bool isAdmin;
+
+        //admin = 0 - kein Administrator
+        //admin = 1 - Administrator
+
+        public User(string username, string vorname, string nachname, string password, string email, bool admin)
+        {
+            this.username = username;
+			this.vorname = vorname;
+			this.nachname = nachname;
+			this.password = password;
+			this.email = email;
+			this.isAdmin = admin;
+        }
+		public void setPassword(string newPassword)
+		{
+			this.password = newPassword;
+		}
+		public void setEmail(string newEmail)
+		{
+			this.email = newEmail;
+		}
+
+		public string getUsername()
+		{
+			return this.username;
+		}
+		public string getVorname()
+		{
+			return this.vorname;
+		}
+		public string getNachname()
+		{
+			return this.nachname;
+		}
+		public string getPassword()
+		{
+			return this.password;
+		}
+		public string getEmail()
+		{
+			return this.email;
+		}
+		public bool getStatus()
+		{
+			return isAdmin;
+		}
+
+	}
+}
