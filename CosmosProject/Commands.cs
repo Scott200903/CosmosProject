d478dbecad2b177dd8a85ba0406c0d1ad65f9645
scott <info@adrag.de>
2024-10-17 15:59:08 +0200

    Implementation of touch, rm, ls, mkdir

---
+++ CosmosProject/Commands.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosProject
+{
+	public class Commands
+	{
+		public string Identifier { get; set; }
+		public int PID { get; set; }
+
+		public virtual void help()
+		{ }
+
+		public virtual void run(string[] payload)
+		{ }
+	} 
+
+	public class MakeDir : Commands 
+	{
+		public MakeDir(string ident, int pid) 
+		{
+			this.Identifier = ident;
+			this.PID = pid;
+		}
+
+		public override void help()
+		{
+			Console.WriteLine("Help from " + Identifier);
+		}
+
+		public override void run(string[] payload)
+		{
+			Console.WriteLine("Run from " + Identifier);
+		}
+	}
+
+}
